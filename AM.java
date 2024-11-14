c940941c6b09497f3374fda068492a316cd198d1
dimitra george <t8230160@gmail.com>
2024-11-14 13:18:46 +0000

    Update AM.java

--- AM.java
+++ AM.java
@@ -7,5 +7,6 @@ public class AM {
         System.out.println("8230115");
         System.out.println("8220085");
         System.out.println("8230055");
+        System.out.println("8230160");
     }
 }
